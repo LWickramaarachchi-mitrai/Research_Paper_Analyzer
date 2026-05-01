bce4ec600ed2e6e415849a69ea96bc4406fc3a0f
Linal Wickramaarachchi <lwickramaarachchi@mitrai.com>
2026-05-01 06:52:33 +0530

    retriever import

--- API/vectorstore/retriever.py
+++ API/vectorstore/retriever.py
@@ -1,22 +1,3 @@
 from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_community.embeddings import HuggingFaceEmbeddings
-#from langchain_text_splitters import RecursiveCharacterTextSplitter
-
-""" model_name = "sentence-transformers/all-MiniLM-L6-v2"
-
-embeddings = HuggingFaceEmbeddings(
-    model_name=model_name
-) """
-
-
-
-""" def retriever(file):
-    loader = PyPDFLoader(file_path= file)
-    pdf = loader.load()
-    splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 100)
-    chunked_doc = splitter.split_documents(pdf)
-    vectorstore = FAISS.from_documents(chunked_doc, embeddings) """
-    
-    
     
