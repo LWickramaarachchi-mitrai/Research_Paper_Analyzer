7a0ea4d0f7d1ca51ee531bb7c94c76539a110b9d
Linal Wickramaarachchi <lwickramaarachchi@mitrai.com>
2026-05-02 23:28:04 +0530

    agent code cleaning

--- API/agents/research_agent.py
+++ API/agents/research_agent.py
@@ -8,15 +8,6 @@ from vectorstore.retriever import save_file
 import json
 load_dotenv()
 
-llm = ChatGroq(
-    model="openai/gpt-oss-20b"
-)
-
-
-
-research_agent_tools = [search_database_tool]
-
-llm_with_tools = llm.bind_tools(research_agent_tools)
 
 llm_structured = ChatGroq(
     model="openai/gpt-oss-120b",
@@ -24,25 +15,8 @@ llm_structured = ChatGroq(
 )
 
 
-
-
 research_system_prompt = research_agent_system_prompt()
 
-
-research_agent = create_agent(
-    model=llm_with_tools,
-    tools = research_agent_tools,
-    system_prompt = research_system_prompt
-)
-
-
-def analyze_paper(paper_path: str,query: str):
-    save_file(paper_path)
-    input = {"messages":[{"role": "user", "content": query}]}
-    result  = research_agent.invoke(input)
-    context = result['messages'][-1].content
-
-    return context
 
 
 def analyze_paper_llm_only(file_path: str):
